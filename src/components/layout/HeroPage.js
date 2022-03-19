abb8097dbcf2c35cd7ccb0a22cc5407f0e710ae4
Cyril Chukwudi Chidolue <cyrilchidolue2016@gmail.com>
2022-03-19 14:35:32 +0100

    Final Commit

--- src/components/layout/HeroPage.js
+++ src/components/layout/HeroPage.js
@@ -6,13 +6,9 @@ const HeroPage = () => {
     <Fragment>
       <section className='text-box' id='text-box'>
         <h1>
-          Bespoke Tech Solutions{' '}
-          <span style={{ color: '#f44336' }}>and Consulting</span>
+          A Fast Growing <span style={{ color: '#f44336' }}> Conglomerate</span>
         </h1>
-        <p>
-          We are one of the best brands that brings your digital dreams to
-          exciting realities.
-        </p>
+        <p>Purring Technology, Purring Merchandise and Purring Capital</p>
         <Link to='/About' className='hero-btn' id='hero-btn'>
           Visit Us To Know More
         </Link>
